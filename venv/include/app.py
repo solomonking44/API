396bfce7a245a9f8cdf9fadfd7e84754810541c0
solomonking44 <mugishasolomon520@gmail.com>
2023-03-28 10:50:08 +0300

    "Present changes"

--- venv/include/app.py
+++ venv/include/app.py
@@ -6,15 +6,39 @@ import json
 app = Flask(__name__)
 api = Api(app)
 
+# @app.route('/')
+# def home():
+    # return render_template('index.html')
+    
+    
+def valid_login(username, password):
+    if username == 'solomon' and password == 'admin':
+        return True
+    else:
+        return False
+
 class Doctor(Resource):
     def get(self):
-        return JSON_FILE
+        return render_template('index.html')
     
-    def post(self, category):
+    def post(self):
+        if request.method == 'POST':
+            name = request.form['username']
+            if valid_login(request.form['username'], request.form['password']):
+                print("welcome")
+                print(request.form['username'])
+                print(request.form['password'])
+                # return render_template('index.html', name=name)
+            else:
+                print("not welcome")
+                print(request.form['username'])
+                print(request.form['password'])
+                # return render_template('navbar.html')
+        return render_template('index.html', name=name)
         
 
-api.add_resource(Doctor, "/doctor")
-api.add_resource(Doctor, "/specialty")
+api.add_resource(Doctor, "/login")
+# api.add_resource(Doctor, "/doctor")
 
 f = open('json2.json')
 
